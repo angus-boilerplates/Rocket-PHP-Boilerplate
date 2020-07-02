7ef0299a615dd90637c4f3de28b8f06e24d619e2
Angus <angus.goody@gmail.com>
2020-07-02 21:22:09 +0100

    Switched to a function to handle NAVBAR current item and started to add page styles

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,12 +4,21 @@
  * for the whole site, updating this file will change
  * the navbar for all pages
  */
+
+//Will set the current navigation page to have ID/CLASS: currentNavItem
+function isCurrent($pageName){
+	global $NAV_PAGE;
+	//If the global matches the argument set as current
+	if ($NAV_PAGE == $pageName){
+		echo "currentNavItem";
+	}
+}
 ?>
 
 <nav>
 	<ul>
-		<li><a href="/index.php">Home</a></li>
-		<li><a href="/page_2.php">Page 2</a></li>
-		<li><a href="/page_3.php">Page 3</a></li>
+		<li id="<?php isCurrent('index')?>"><a href="/index.php">Home</a></li>
+		<li id="<?php isCurrent('page_2')?>"><a href="/page_2.php">Page 2</a></li>
+		<li id="<?php isCurrent('page_3')?>"><a href="/page_3.php">Page 3</a></li>
 	</ul>
 </nav>
