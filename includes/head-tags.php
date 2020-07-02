18a6f5dcdfcf7c3de008c5f264867cdf07b2e1f6
Angus <angus.goody@gmail.com>
2020-07-02 20:10:23 +0100

    Added a template Head tags file

---
+++ includes/head-tags.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This is the Head-Tags file, this is where you should place HTML tags
+ * You want to appear on all your pages, such as Canonical, Stylesheets,
+ * Descriptions, Viewports etc
+ *
+ */
+?>
+<title><?php print $PAGE_TITLE;?></title>
+<!--Meta information-->
+<meta name="description" content="<?php print $PAGE_DESCRIPTION;?>">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="keywords" content="example,tags,go,here">
+<meta name="geo.placename" content="location">
+<meta name="author" content="Angus Goody">
+<!--Canonical-->
+<link rel="canonical" href="<?php echo $PAGE_CANONICAL ?>"/>
+<!--Favicons-->
+<link rel="shortcut icon" type="image/png" href=""/>
+<link rel="apple-touch-icon" href="">
