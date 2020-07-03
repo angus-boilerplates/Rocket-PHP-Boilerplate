ae66bdfb2de420b27a71c6df6e9af9ea758035bb
Angus <angus.goody@gmail.com>
2020-07-03 15:47:00 +0100

    Restyled comments in CSS and fixed footer bug

--- page_3.php
+++ page_3.php
@@ -12,6 +12,8 @@
 	<!-- Content -->
 	<div id="wrapper">
 		<h1 id="pageheader"> PAGE 3 </h1>
+		<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eleifend ornare neque vel rhoncus. Integer aliquam, quam vitae vehicula scelerisque, nisi libero fermentum tellus, a vehicula leo turpis id ligula. In vehicula tempor nunc ut porttitor. Proin tincidunt ligula non odio pharetra, sit amet lacinia metus sagittis. In non tristique turpis. Maecenas dignissim mi quis ligula accumsan, eget pulvinar arcu bibendum. Donec magna leo, semper sed mi vel, tempor porttitor justo. Curabitur euismod nulla eros, et tristique lectus sodales eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc in urna a quam faucibus tempus. Aenean vitae tellus a felis placerat ultricies. Aenean bibendum vestibulum libero, vitae mollis ipsum.</p>
 	</div>
 	<!-- Footer -->
 	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/footer.php";include_once($path); ?>
