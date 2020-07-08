6d7528e4307ac040b13e6deab6ff9253f8edf947
Angus <angus.goody@gmail.com>
2020-07-08 13:39:44 +0100

    Updated includes to be more efficient

--- page_2.php
+++ page_2.php
@@ -1,19 +1,19 @@
-<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/core/a_config.php";include_once($path); ?>
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/core/a_config.php");?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<!-- Head tags -->
-	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/head-tags.php";include_once($path); ?>
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/head-tags.php");?>
 </head>
 
 <body>
 	<!-- Navbar -->
-	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/navbar.php";include_once($path); ?>
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/navbar.php");?>
 	<!-- Content -->
 	<div id="wrapper">
 		<h1 id="pageheader"> PAGE 2 </h1>
 	</div>
 	<!-- Footer -->
-	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/footer.php";include_once($path); ?>
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
 </body>
 </html>
--- page_3.php
+++ page_3.php
@@ -1,14 +1,14 @@
-<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/core/a_config.php";include_once($path); ?>
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/core/a_config.php");?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<!-- Head tags -->
-	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/head-tags.php";include_once($path); ?>
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/head-tags.php");?>
 </head>
 
 <body>
 	<!-- Navbar -->
-	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/navbar.php";include_once($path); ?>
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/navbar.php");?>
 	<!-- Content -->
 	<div id="wrapper">
 		<h1 id="pageheader"> PAGE 3 </h1>
@@ -16,6 +16,6 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eleifend ornare neque vel rhoncus. Integer aliquam, quam vitae vehicula scelerisque, nisi libero fermentum tellus, a vehicula leo turpis id ligula. In vehicula tempor nunc ut porttitor. Proin tincidunt ligula non odio pharetra, sit amet lacinia metus sagittis. In non tristique turpis. Maecenas dignissim mi quis ligula accumsan, eget pulvinar arcu bibendum. Donec magna leo, semper sed mi vel, tempor porttitor justo. Curabitur euismod nulla eros, et tristique lectus sodales eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc in urna a quam faucibus tempus. Aenean vitae tellus a felis placerat ultricies. Aenean bibendum vestibulum libero, vitae mollis ipsum.</p>
 	</div>
 	<!-- Footer -->
-	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/footer.php";include_once($path); ?>
+	<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/footer.php");?>
 </body>
 </html>
